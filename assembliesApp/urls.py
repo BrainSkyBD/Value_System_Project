9441ef7732f999fcc9e6e36c8ad560f9afe90265
abdurrahmanc-dev <abdurrahmanc@lambdacrunch.com>
2025-01-26 22:56:22 +0600

    add edit in html

--- assembliesApp/urls.py
+++ assembliesApp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('edit_assembly/<int:pk>/', views.edit_assembly, name="edit_assembly"),
     path('assemblies_Delete/<int:pk>/', views.assemblies_Delete, name="assemblies_Delete"),
 
+    path('delete-resource/', views.delete_resource, name='delete_resource'),
+
     path('Assemblies_Code_L1_module/', views.Assemblies_Code_L1_module, name="Assemblies_Code_L1_module"),
     path('Assemblies_Code_L2_module/', views.Assemblies_Code_L2_module, name="Assemblies_Code_L2_module"),
     path('Assemblies_Code_L3_module/', views.Assemblies_Code_L3_module, name="Assemblies_Code_L3_module"),
